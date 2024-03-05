6b7a8c2f043ebd0c96c753f8d3700ae0dfdb4bd7
Zonirina Mick Ramahazomanana <zramahaz@c1r11s4.42antananarivo.mg>
2024-03-05 16:20:25 +0300

    fin des parties obligatoire

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: zramahaz <zramahaz@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 15:32:15 by zramahaz          #+#    #+#             */
-/*   Updated: 2024/02/25 15:45:02 by zramahaz         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:08:15 by zramahaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,5 @@ int	main()
 
 void	ft_bzero(void *str, size_t size)
 {
-	while (size--)
-		*(char *)str++ = '\0';
+	ft_memset(str, '\0', size);
 }
